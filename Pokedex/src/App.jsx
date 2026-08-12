9b2102650225ca771e692a2f1f3671fc8610e972
Mahatana Rakotomalala <hei.mahatana@gmail.com>
2026-08-13 01:34:50 +0300

    add the wiring in the App

--- Pokedex/src/App.jsx
+++ Pokedex/src/App.jsx
@@ -1,7 +1,38 @@
 import './App.css'
+import { fetchPokemon } from './services/pokeapi'
+import { useEffect, useState } from 'react'
+import PokemonList from './components/PokemonList';
 
 function App() {
-  
+  const [pokemons, setPokemons] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function loadPokemons () {
+      try {
+        setIsLoading(true);
+        const data = await fetchPokemon();
+        setPokemons(data);
+      } catch (err){
+        setError(err.message)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    loadPokemons();
+  }, []);
+
+  if (isLoading) return <p className="status-message">Loading Pokédex...</p>;
+  if (error) return <p className="status-message error">Error: {error}</p>;
+
+  return (
+    <div className="app">
+      <h1>Pokédex</h1>
+      <PokemonList pokemons={pokemons} />
+    </div>
+  );
 }
 
 export default App
