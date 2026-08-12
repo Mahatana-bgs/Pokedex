343067eb6677a4dbfadecdf1d2874ee9cadea6b9
Mahatana Rakotomalala <hei.mahatana@gmail.com>
2026-08-13 01:03:36 +0300

    resolving of the url

--- Pokedex/src/services/pokeapi.js
+++ Pokedex/src/services/pokeapi.js
@@ -1,7 +1,7 @@
 const listAPI = "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0";
 
 export async function fetchPokemon(){
-    const listResponse = await fetch('${listAPI}/pokemon?limit=100&offset=0');
+    const listResponse = await fetch(`${listAPI}/pokemon?limit=100&offset=0`);
     if(!listResponse.ok){
         throw new Error("Failed to fetch the Pokemon list");
     }
